3ab5cea46f68c3dc0c01f4150340f27d1559f2b7
Laura Strickland <laura.strickland@gtri.gatech.edu>
2016-10-04 16:01:34 -0400

    Trying to get terminator config set up in new_machine_setup

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -4,5 +4,5 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade
 
 sudo apt-get install vim vim-gnome terminator synaptic
 
-
+mkdir ~/.config/terminator && cp ./config ~/.config/terminator
 
