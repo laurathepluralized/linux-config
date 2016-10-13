ab12abcc2ee5df6c6d07dc3969e230b66c5a35d8
laura <laura.strickland@gtri.gatech.edu>
2016-10-13 16:05:35 -0400

    Fixed wrong oh-my-zsh env var and added some others

--- .zshrc
+++ .zshrc
@@ -1,3 +1,5 @@
+
+export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="miloshadzic"
 plugins=(git bundler osx rake ruby)
 CASE_SENSITIVE="true"
--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,3 +1,4 @@
+LCONFIG=$HOME/linux-config
 
 # apt-get
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade
@@ -6,27 +7,32 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade
 sudo apt-get install vim vim-gnome terminator git meld synaptic zsh zsh-common mercurial
 
 mkdir -p ~/.config/terminator
-cp ./config ~/.config/terminator
+cp $LCONFIG/config ~/.config/terminator
+
+
+if [ -f $HOME/.zshrc ]; 
+    then mv $HOME/.zshrc $HOME/.zshrc.orig
+fi
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cp ~/linux-config/.zshrc ~/.zshrc
 
 if [ ! -f $HOME/.bash_aliases ]; 
-    then cp ./.bash_aliases $HOME/.bash_aliases
-    else cat ./.bash_aliases >> $HOME/.bash_aliases
+    then cp $LCONFIG/.bash_aliases $HOME/.bash_aliases
+    else "source $HOME/linux-config/.bash_aliases" >> $HOME/.bash_aliases
 fi
 if [ ! -f $HOME/.zsh_aliases ]; 
-    then cp ./.zsh_aliases $HOME/.zsh_aliases
-    else cat ./.bash_aliases >> $HOME/.bash_aliases
+    then cp $LCONFIG/.zsh_aliases $HOME/.zsh_aliases
+    else "source $HOME/linux-config/.zsh_aliases" >> $HOME/.zsh_aliases
 fi
 
 if [ ! -f $HOME/.inputrc ];
-    then cp ./.inputrc $HOME/.inputrc
-    else cat ./.inputrc >> $HOME/.inputrc
+    then cp $LCONFIG/.inputrc $HOME/.inputrc
+    else "source $HOME/linux-config/.inputrc" >> $HOME/.inputrc
 fi
 
+echo "source ~/linux-config/.zshrc" >> ~/.zshrc
 echo "source ~/linux-config/.vimrc" >> ~/.vimrc
 mkdir -p $HOME/.vim/
 mkdir -p $HOME/.vim/backups
@@ -38,46 +44,46 @@ echo "set dir=~/.vim/swaps" >> ~/.vimrc
 
 ### vim plugins
 mkdir -p $HOME/vim_plugins
-DIR=$HOME/vim_plugins/
+DIR=$HOME/vim_plugins
 mkdir -p $HOME/.vim/autoload
 mkdir -p $HOME/.vim/bundle
 
 #pathogen:
 
-    git clone https://github.com/tpope/vim-pathogen.git
+    git clone https://github.com/tpope/vim-pathogen.git $DIR/vim-pathogen
     ln -sf $DIR/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
 
 #togglelist:
 
-    git clone https://github.com/milkypostman/vim-togglelist
+    git clone https://github.com/milkypostman/vim-togglelist $DIR/vim-togglelist
     ln -sf $DIR/vim-togglelist ~/.vim/bundle/
 
 #lvdb:
 
-    git clone https://github.com/esquires/lvdb
+    git clone https://github.com/esquires/lvdb $DIR/lvdb
     ln -sf $DIR/lvdb ~/.vim/bundle/
 
 #tabcity:
-    git clone https://github.com/esquires/tabcity
+    git clone https://github.com/esquires/tabcity $DIR/tabcity
     ln -sf $DIR/tabcity ~/.vim/bundle
 
 #vim-map-medley:
 
-    git clone https://github.com/esquires/vim-map-medley
+    git clone https://github.com/esquires/vim-map-medley $DIR/vim-map-medley
     ln -sf $DIR/vim-map-medley ~/.vim/bundle/
 
 #syntastic:
 
-    git clone https://github.com/scrooloose/syntastic.git
+    git clone https://github.com/scrooloose/syntastic.git $DIR/syntastic
     ln -sf $DIR/syntastic ~/.vim/bundle
 
 #vim-l9:
     # installed as a dependency for vim-fuzzyfinder
-    hg clone https://bitbucket.org/ns9tks/vim-l9
+    hg clone https://bitbucket.org/ns9tks/vim-l9 $DIR/vim-l9
     ln -sf $DIR/vim-l9 ~/.vim/bundle
 
 #vim-fuzzyfinder:
-    hg clone https://bitbucket.org/ns9tks/vim-fuzzyfinder
+    hg clone https://bitbucket.org/ns9tks/vim-fuzzyfinder $DIR/vim-fuzzyfinder
     ln -sf $DIR/vim-fuzzyfinder ~/.vim/bundle
 
 #youcompleteme:
@@ -87,5 +93,5 @@ mkdir -p $HOME/.vim/bundle
     sudo apt install vim-youcompleteme && vam install youcompleteme
     # vam = vim addons manager
 
-
+zsh
 source ~/.zshrc
