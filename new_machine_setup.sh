1f97c069564f562ab21edef620ce817d3524948a
laura <laura.strickland@gtri.gatech.edu>
2016-10-13 14:52:02 -0400

    Added -p flag to mkdirs and copies my zshrc to home dir

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -5,11 +5,13 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade
 # install the essential utilities
 sudo apt-get install vim vim-gnome terminator synaptic zsh zsh-common
 
+mkdir -p ~/.config/terminator
+cp ./config ~/.config/terminator
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-mkdir ~/.config/terminator
-cp ./config ~/.config/terminator
+cp ~/linux-config/.zshrc ~/.zshrc
 
 if [ ! -f $HOME/.bash_aliases ]; 
     then cp ./.bash_aliases $HOME/.bash_aliases
@@ -21,9 +23,9 @@ if [ ! -f $HOME/.zsh_aliases ];
 fi
 
 echo "source ~/linux-config/.vimrc" >> ~/.vimrc
-mkdir $HOME/.vim/
-mkdir $HOME/.vim/backups
-mkdir $HOME/.vim/swaps
+mkdir -p $HOME/.vim/
+mkdir -p $HOME/.vim/backups
+mkdir -p $HOME/.vim/swaps
 echo "set backup" >> ~/.vimrc
 echo "set backupdir=~/.vim/backups" >> ~/.vimrc
 echo "set dir=~/.vim/swaps" >> ~/.vimrc
