f300926f23c44fd9000510c3d470afcaf3cc8da8
Laura Strickland <laura.strickland@gtri.gatech.edu>
2017-10-30 20:13:33 -0400

    Powerline in awesome menubar and starting new custom theme

--- gen_config.lua
+++ gen_config.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("~/repos/linux-config/laura_awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 editor = os.getenv("EDITOR") or "vim"
