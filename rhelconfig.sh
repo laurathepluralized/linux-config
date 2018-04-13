364d2654e732a3a5590db237e1a12f9e4b2edce6
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-13 11:22:08 -0400

    fixing git pull or clone stuff, attempt at non-sudo-requiring script for rhel

--- rhelconfig.sh
+++ rhelconfig.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# Usage: bash ubuntu_install.sh
+# Usage: bash rhelconfig.sh
 
 clone_or_pull () {
     # THEURL is assumed to be the git url
@@ -34,79 +34,7 @@ hg_clone_or_pull () {
     return 0
 }
 
-# Borrowing technique from here for checking for rootness
-# http://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script/21622456#21622456
-# The sudo method they suggest may also be useful
-if (( $EUID == 0 ))
-    then echo "Please do not run this script as root"
-    exit
-else
-    ME=`(whoami)`
-    echo "Current user is ${ME}"
-fi
-
-sudo apt update
-sudo apt upgrade
-sudo apt install -y \
-	awesome \
-    cmake \
-    curl \
-    exuberant-ctags \
-	git \
-    global \
-    htop \
-    ipython \
-    libgtk2.0-dev \
-    libnotify-dev \
-	mercurial \
-    ncurses-cmake-gui \
-    pcmanfm \
-    python-dev \
-    python3-dev \
-    python-ipdb \
-    terminator \
-	vim-gnome \
-	zsh
-
-sudo add-apt-repository 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-4.0 main'
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-    #clang-4.0 clang-4.0-doc libclang-common-4.0-dev libclang-4.0-dev libclang1-4.0 libclang1-4.0-dbg libllvm-4.0-ocaml-dev libllvm4.0 libllvm4.0-dbg lldb-4.0 llvm-4.0 llvm-4.0-dev llvm-4.0-doc llvm-4.0-examples llvm-4.0-runtime clang-format-4.0 python-clang-4.0 lldb-4.0-dev lld-4.0 libfuzzer-4.0-dev libclang-4.0
-yes | sudo apt-get update && sudo apt-get install libclang-4.0 libclang-4.0-dev liblldb-4.0 liblldb-4.0-dbg liblldb-4.0-dev
-	#vim-youcompleteme   # If this is installed with apt or apt-get, it will install the version that requires clang-3.9+ but 
-                         # that has clang-3.8 listed as a required dependency. Not sure what happened there. 
-
-# NOTE: clang-4.0 is included here due to the most up-to-date version of YouCompleteMe available 
-# for Ubuntu 16.04 requires clang-3.9 or later. The clang repository added earlier throws up a 
-# bunch of unsigned repo warnings, but that repo is in fact the official way to install a later 
-# libclang than Ubuntu's suggested version, so no worries.
-
 CONFIG_DIR=${HOME}"/repos/linux-config"
-
-### TODO: Fix checking whether powerline is installed, or add command-line arg for whether 
-#to do powerline font installation and such
-# if [ ! sudo python3 -m pip install --user powerline-status ]; then
-#     # we're just going to do pretty much every font installation procedure powerline offers
-#     pushd ${CONFIG_DIR} && \
-#         git clone https://github.com/powerline/fonts.git --depth=1 \
-#         && pushd fonts \
-#         && ./install.sh \
-#         && popd && popd
-# 
-#     wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-#     wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-#     FONTPATH=/usr/share/fonts/X11/misc/PowerlineSymbols.otf
-#     sudo mv PowerlineSymbols.otf ${FONTPATH}
-#     sudo fc-cache -vf ${FONTPATH}
-#     FONTCONFIGPATH=~/.config/fontconfig/conf.d
-#     mkdir -p ${FONTCONFIGPATH}
-#     mv 10-powerline-symbols.conf ${FONTCONFIGPATH}
-#     echo "For powerline's symbols to work correctly, restart x once this script finishes."
-#     sudo ln -sfn ${CONFIG_DIR}/laura_awesome /usr/share/awesome/themes/laura_awesome
-#     sudo mv /usr/local/lib/python3.5/dist-packages/powerline/config_fiiles/themes/wm/default.json /usr/local/lib/python3.5/dist-packages/powerline/config_fiiles/themes/wm/default.old
-#     sudo ln -sfn ${CONFIG_DIR}/powerline_wm_default.json /usr/local/lib/python3.5/dist-packages/powerline/config_fiiles/themes/wm/default.json
-# else
-#     echo "powerline already installed, so assuming fonts and themes are already configured"
-# fi
 
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
@@ -236,8 +164,8 @@ clone_or_pull
 ln -sfn ${DIR}/vim-commentary ${DOTVIM}/bundle
 
 # Install my colorschemes
-sudo ln -sfn ${CONFIG_DIR}/laura.vim /usr/share/vim/vim74/colors/laura.vim
-sudo ln -sfn ${CONFIG_DIR}/laura_light.vim /usr/share/vim/vim74/colors/laura_light.vim
+# ln -sfn ${CONFIG_DIR}/laura.vim /usr/share/vim/vim74/colors/laura.vim
+# ln -sfn ${CONFIG_DIR}/laura_light.vim /usr/share/vim/vim74/colors/laura_light.vim
 
 # # The next few lines, which install YouCompleteMe from source, assume
 # # that the version of vim installed was compiled to use python3, not python2.
@@ -269,5 +197,5 @@ cd ${PRE_YCM_INSTALL_DIR}
 echo "Now changing the following user's default shell to zsh:"
 echo ${ME}
 
-sudo chsh -s /usr/bin/zsh ${ME}
+chsh -s /usr/bin/zsh ${ME}
 
