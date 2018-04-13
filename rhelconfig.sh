d81ae41da731aa049ec401dbeb5d60ad6b979036
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-13 11:37:17 -0400

    removing some stuff that rhel doesn't like or that I don't use anymore

--- rhelconfig.sh
+++ rhelconfig.sh
@@ -72,10 +72,6 @@ mkdir -p ${DIR}
 chown -R ${ME}:${ME} ${DIR}
 pushd ${DIR}
 
-
-
-
-
 THEURL=https://github.com/tpope/vim-pathogen.git
 REPONAME=vim-pathogen
 clone_or_pull
@@ -101,11 +97,11 @@ REPONAME=vim-map-medley
 clone_or_pull
 ln -sfn ${DIR}/vim-map-medley ${DOTVIM}/bundle/
 
-THEURL=https://github.com/chrisbra/csv.vim.git
-REPONAME=csv.vim
-clone_or_pull
-ln -sfn ${DIR}/csv.vim ${DOTVIM}/bundle/
-ln -sfn ${CONFIG_DIR}/filetype.vim ${DOTVIM}/filetype.vim
+# THEURL=https://github.com/chrisbra/csv.vim.git
+# REPONAME=csv.vim
+# clone_or_pull
+# ln -sfn ${DIR}/csv.vim ${DOTVIM}/bundle/
+# ln -sfn ${CONFIG_DIR}/filetype.vim ${DOTVIM}/filetype.vim
 
 THEURL=https://github.com/scrooloose/syntastic.git
 REPONAME=syntastic
@@ -117,36 +113,11 @@ REPONAME=ctrlp.vim
 clone_or_pull
 ln -sfn ${DIR}/ctrlp.vim ${DOTVIM}/bundle/
 
-# syntax highlighting for .scala files (needed for one homework assignment, 
-# maybe never again?  )
-# THEURL=git@github.com:derekwyatt/vim-scala.git
-# REPONAME=vim-scala
-# clone_or_pull
-# ln -sfn ${DIR}/vim-scala ${DOTVIM}/bundle/
-# if [ ! git clone https://github.com/derekwyatt/vim-fswitch.git ]; then
-    # pushd vim-fswitch && git stash && git pull origin master && git stash apply
-    # popd
-# else
-    # ln -sfn ${DIR}/vim-fswitch ${DOTVIM}/bundle
-# fi
-
 # From https://superuser.com/questions/219009/how-do-i-move-around-and-otherwise-rearrange-splits-in-vim
 THEURL=https://github.com/wesQ3/vim-windowswap.git
 REPONAME=vim-windowswap
 clone_or_pull
 ln -sfn ${DIR}/vim-windowswap ${DOTVIM}/bundle
-
-# if [ ! git clone https://github.com/scrooloose/nerdcommenter.git ]; then
-    # pushd nerdcommenter && git stash && git pull origin master && git stash apply
-    # popd
-# else
-    # ln -sfn ${DIR}/nerdcommenter ${DOTVIM}/bundle
-# fi
-
-THEURL=https://github.com/jsfaint/gen_tags.vim.git
-REPONAME=gen_tags.vim
-clone_or_pull
-ln -sfn ${DIR}/gen_tags.vim ${DOTVIM}/bundle
 
 THEURL=https://github.com/tpope/tpope-vim-abolish.git
 REPONAME=tpope-vim-abolish
@@ -164,38 +135,12 @@ clone_or_pull
 ln -sfn ${DIR}/vim-commentary ${DOTVIM}/bundle
 
 # Install my colorschemes
-# ln -sfn ${CONFIG_DIR}/laura.vim /usr/share/vim/vim74/colors/laura.vim
-# ln -sfn ${CONFIG_DIR}/laura_light.vim /usr/share/vim/vim74/colors/laura_light.vim
+mkdir -p ${HOME}/.vim/colors
+sudo ln -sfn ${CONFIG_DIR}/laura.vim ${HOME}/.vim/colors/laura.vim
+sudo ln -sfn ${CONFIG_DIR}/laura_light.vim ${HOME}/.vim/colors/laura_light.vim
 
-# # The next few lines, which install YouCompleteMe from source, assume
-# # that the version of vim installed was compiled to use python3, not python2.
-PRE_YCM_INSTALL_DIR=$(pwd)
-THEURL=https://github.com/Valloric/YouCompleteMe.git
-REPONAME=YouCompleteMe
-clone_or_pull
-ln -sfn ${DIR}/YouCompleteMe ${DOTVIM}/bundle
-# Make sure we own all the stuff in ${DIR}
-chown -R ${ME}:${ME} ${DIR}
-pushd ${DIR}
-pushd YouCompleteMe
-git submodule update --init --recursive
+# echo "Now changing the following user's default shell to zsh:"
+# echo ${ME}
 
-# this is where YouCompleteMe's build files will go
-mkdir -p ${HOME}/ycm_build && pushd ${HOME}/ycm_build
-chown -R ${ME}:${ME} ${HOME}/ycm_build
-
-# This should generate CMake files for clang-4.0 installed in default directory
-# and for Vim compiled to use Python3 (since YCM has to use the same Python version Vim does)
-cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=/usr/lib/x86_64-linux-gnu/libclang-4.0.so -DUSE_PYTHON2=OFF . ${DIR}/YouCompleteMe/third_party/ycmd/cpp
-
-# Now build YCM
-cmake --build . --target ycm_core --config Release
-
-# go back to original directory
-cd ${PRE_YCM_INSTALL_DIR}
-
-echo "Now changing the following user's default shell to zsh:"
-echo ${ME}
-
-chsh -s /usr/bin/zsh ${ME}
+# chsh -s /usr/bin/zsh ${ME}
 
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -144,10 +144,6 @@ mkdir -p ${DIR}
 chown -R ${ME}:${ME} ${DIR}
 pushd ${DIR}
 
-
-
-
-
 THEURL=https://github.com/tpope/vim-pathogen.git
 REPONAME=vim-pathogen
 clone_or_pull
@@ -173,12 +169,6 @@ REPONAME=vim-map-medley
 clone_or_pull
 ln -sfn ${DIR}/vim-map-medley ${DOTVIM}/bundle/
 
-THEURL=https://github.com/chrisbra/csv.vim.git
-REPONAME=csv.vim
-clone_or_pull
-ln -sfn ${DIR}/csv.vim ${DOTVIM}/bundle/
-ln -sfn ${CONFIG_DIR}/filetype.vim ${DOTVIM}/filetype.vim
-
 THEURL=https://github.com/scrooloose/syntastic.git
 REPONAME=syntastic
 clone_or_pull
@@ -189,31 +179,11 @@ REPONAME=ctrlp.vim
 clone_or_pull
 ln -sfn ${DIR}/ctrlp.vim ${DOTVIM}/bundle/
 
-# syntax highlighting for .scala files (needed for one homework assignment, 
-# maybe never again?  )
-# THEURL=git@github.com:derekwyatt/vim-scala.git
-# REPONAME=vim-scala
-# clone_or_pull
-# ln -sfn ${DIR}/vim-scala ${DOTVIM}/bundle/
-# if [ ! git clone https://github.com/derekwyatt/vim-fswitch.git ]; then
-    # pushd vim-fswitch && git stash && git pull origin master && git stash apply
-    # popd
-# else
-    # ln -sfn ${DIR}/vim-fswitch ${DOTVIM}/bundle
-# fi
-
 # From https://superuser.com/questions/219009/how-do-i-move-around-and-otherwise-rearrange-splits-in-vim
 THEURL=https://github.com/wesQ3/vim-windowswap.git
 REPONAME=vim-windowswap
 clone_or_pull
 ln -sfn ${DIR}/vim-windowswap ${DOTVIM}/bundle
-
-# if [ ! git clone https://github.com/scrooloose/nerdcommenter.git ]; then
-    # pushd nerdcommenter && git stash && git pull origin master && git stash apply
-    # popd
-# else
-    # ln -sfn ${DIR}/nerdcommenter ${DOTVIM}/bundle
-# fi
 
 THEURL=https://github.com/jsfaint/gen_tags.vim.git
 REPONAME=gen_tags.vim
@@ -236,8 +206,9 @@ clone_or_pull
 ln -sfn ${DIR}/vim-commentary ${DOTVIM}/bundle
 
 # Install my colorschemes
-sudo ln -sfn ${CONFIG_DIR}/laura.vim /usr/share/vim/vim74/colors/laura.vim
-sudo ln -sfn ${CONFIG_DIR}/laura_light.vim /usr/share/vim/vim74/colors/laura_light.vim
+mkdir -p ${HOME}/.vim/colors
+sudo ln -sfn ${CONFIG_DIR}/laura.vim ${HOME}/.vim/colors/laura.vim
+sudo ln -sfn ${CONFIG_DIR}/laura_light.vim ${HOME}/.vim/colors/laura_light.vim
 
 # # The next few lines, which install YouCompleteMe from source, assume
 # # that the version of vim installed was compiled to use python3, not python2.
